74ac05094238756c3bc98bbabf3f7ae65254957f
Trevor <wtrevor242@gmail.com>
2021-07-18 20:28:36 -0600

    Added move counter logic

--- js/battle.js
+++ js/battle.js
@@ -1,31 +1,26 @@
 var compHPBAR = document.getElementById("compHPBAR")
 var myHPBAR = document.getElementById("myHPBAR")
 
+//Retrieve our cookies and parse them
 var getCookie = Cookies.get('pokeState');
-console.log(getCookie);
 var getCookieComp = Cookies.get('pokeComp');
-console.log(getCookieComp);
-
 var cookieParse = JSON.parse(getCookie);
 var cookieParseComp = JSON.parse(getCookieComp);
 
-appendtoBattle();
 function appendtoBattle() {
     //Create elements in DOM
     var selectedImg = document.createElement('img');
     var selectedName = document.createElement('h2');
     var parent = document.getElementById('parent');
-
+    var firstChild = parent.firstElementChild;
     //setting the values for each created element and append to DOM
     selectedImg.setAttribute('src', cookieParse.pokemonPic);
-    parent.append(selectedImg);
+    parent.insertBefore(selectedImg, firstChild);
 
     selectedName.innerText = cookieParse.pokemonName;
-    parent.append(selectedName);
-
-    // selectedCurrHP.innerText = "Current Health: "+cookieParse.userCurrentHealth;
-    // parent.append(selectedCurrHP);
+    parent.insertBefore(selectedName, firstChild);
 }
+appendtoBattle();
 
 var pokemonCompState = {
     pokemonName : 'Pikachu',
@@ -34,37 +29,38 @@ var pokemonCompState = {
         {
         attackName: "Lightning Bolt",
         attackValue: 12,
-            availableable: {
-                remainingAttacks: 10,
-                totalAttacks: 10
+            available: {
+            remainingAttacks: 10,
+            totalAttacks: 10
             }
         },
         {
         attackName: "Quick Attack",
         attackValue: 10,
-            availableable: {
-                remainingAttacks: 10,
-                totalAttacks: 10
+        available: {
+            remainingAttacks: 10,
+            totalAttacks: 10
             }
         },
         {
         attackName: "Double Kick",
         attackValue: 7,
-            availableable: {
-                remainingAttacks: 10,
-                totalAttacks: 10
+        available: {
+            remainingAttacks: 10,
+            totalAttacks: 10
             }
         },
         {
         attackName: "Thunder Punch",
         attackValue: 17,
-            availableable: {
-                remainingAttacks: 10,
-                totalAttacks: 10
+        available: {
+            remainingAttacks: 10,
+            totalAttacks: 10
             }
         }
     ]
 }
+
 // PLAYER ATTACK ONE
 var attackClick = document.getElementById('Attack1');
 attackClick.addEventListener('click', playerTurn);
@@ -91,7 +87,6 @@ var attackClick = document.getElementById('Attack2');
 attackClick.addEventListener('click', playerTurn);
 
 function playerTurn() {
-    
     var damageDealt = Math.floor(Math.random() * 11);
     console.log("Damage dealt: "+ damageDealt);
     
@@ -154,7 +149,6 @@ function playerTurn() {
 }
 
 function computerTurn() {
-    //console.log("test");
     var compAttIndex = Math.floor(Math.random() * (pokemonCompState.attack.length));
     console.log("Computer Choose attack " + compAttIndex);
     console.log("Computer Choose move " + pokemonCompState.attack[compAttIndex].attackName);
@@ -172,78 +166,216 @@ function computerTurn() {
     }
 }
 
-function moveAvailable() {
-    if (document.getElementById('Attack1').clicked == true) {
-
-  }
-}
 function reDirecttoHome() {
     document.location.href="/index.html"; 
 }
 
-button1 = document.getElementById('Attack1');
-button2 = document.getElementById('Attack2');
-button3 = document.getElementById('Attack3');
-button4 = document.getElementById('Attack4');
+const button1 = document.getElementById('Attack1');
+const button2 = document.getElementById('Attack2');
+const button3 = document.getElementById('Attack3');
+const button4 = document.getElementById('Attack4');
 
 if(cookieParse.pokemonName == "Bulbasaur") {
     // BULBASAUR MOVESET
     var bulbMoves = [
         {
         name: "tackle",
+        available: {
+            remainingAttacks: 10,
+            totalAttacks: 10
+            }
         },
         {
         name: "vine whip",
+        available: {
+            remainingAttacks: 20,
+            totalAttacks: 20
+            }
         },
         {
         name: "razor leaf",
+        available: {
+            remainingAttacks: 5,
+            totalAttacks: 5
+            }
         },
         {
         name: "solar beam",
+        available: {
+            remainingAttacks: 5,
+            totalAttacks: 5
+            }
         }
     ]
-    button1.innerText = bulbMoves[0].name ;
-    button2.innerText = bulbMoves[1].name ;
-    button3.innerText = bulbMoves[2].name ;
-    button4.innerText = bulbMoves[3].name ; 
-} else if(cookieParse.pokemonName == "Charmander") {
+
+    function reduceAvailablility1(move) {
+        bulbMoves[move].available.remainingAttacks -= 1;
+        button1.innerText = bulbMoves[move].name+" "+bulbMoves[move].available.remainingAttacks+" "+"/"+bulbMoves[move].available.totalAttacks;
+    }
+    function reduceAvailablility2(move) {
+        bulbMoves[move].available.remainingAttacks -= 1;
+        button2.innerText = bulbMoves[move].name+" "+bulbMoves[move].available.remainingAttacks+" "+"/"+bulbMoves[move].available.totalAttacks;
+    }
+    function reduceAvailablility3(move) {
+        bulbMoves[move].available.remainingAttacks -= 1;
+        button3.innerText = bulbMoves[move].name+" "+bulbMoves[move].available.remainingAttacks+" "+"/"+bulbMoves[move].available.totalAttacks;
+    }
+    function reduceAvailablility4(move) {
+        bulbMoves[move].available.remainingAttacks -= 1;
+        button4.innerText = bulbMoves[move].name+" "+bulbMoves[move].available.remainingAttacks+" "+"/"+bulbMoves[move].available.totalAttacks;
+    }
+
+    button1.innerText = bulbMoves[0].name+" "+bulbMoves[0].available.remainingAttacks+" "+"/"+bulbMoves[0].available.totalAttacks;
+    button1.addEventListener('click', function() {
+        reduceAvailablility1(0);
+    });
+    
+    button2.innerText = bulbMoves[1].name+" "+bulbMoves[1].available.remainingAttacks+" "+"/"+bulbMoves[1].available.totalAttacks;
+    button2.addEventListener('click', function() {
+        reduceAvailablility2(1);
+    });
+
+    button3.innerText = bulbMoves[2].name+" "+bulbMoves[2].available.remainingAttacks+" "+"/"+bulbMoves[2].available.totalAttacks;
+    button3.addEventListener('click', function() {
+        reduceAvailablility3(2);
+    });
+
+    button4.innerText = bulbMoves[3].name+" "+bulbMoves[3].available.remainingAttacks+" "+"/"+bulbMoves[3].available.totalAttacks;
+    button4.addEventListener('click', function() {
+        reduceAvailablility4(3);
+    });
+    
+}else if(cookieParse.pokemonName == "Charmander") {
     // CHARMANDER MOVESET
     var charMoves = [
         {
         name: "tackle",
+        available: {
+            remainingAttacks: 10,
+            totalAttacks: 10
+            }
         },
         {
         name: "ember",
+        available: {
+            remainingAttacks: 10,
+            totalAttacks: 10
+            }
         },
         {
         name: "flame thrower",
+        available: {
+            remainingAttacks: 5,
+            totalAttacks: 5
+            }
         },
         {
         name: "bite",
+        available: {
+            remainingAttacks: 5,
+            totalAttacks: 5
+            }
         }
     ]
-    button1.innerText = charMoves[0].name ;
-    button2.innerText = charMoves[1].name ;
-    button3.innerText = charMoves[2].name ;
-    button4.innerText = charMoves[3].name;
+
+    function reduceAvailablility(move) {
+        charMoves[move].available.remainingAttacks -= 1;
+        button1.innerText = charMoves[move].name+" "+charMoves[move].available.remainingAttacks+" "+"/"+charMoves[move].available.totalAttacks;
+    }
+    function reduceAvailablility2(move) {
+        charMoves[move].available.remainingAttacks -= 1;
+        button2.innerText = charMoves[move].name+" "+charMoves[move].available.remainingAttacks+" "+"/"+charMoves[move].available.totalAttacks;        
+    }
+    function reduceAvailablility3(move) {
+        charMoves[move].available.remainingAttacks -= 1;
+        button3.innerText = charMoves[move].name+" "+charMoves[move].available.remainingAttacks+" "+"/"+charMoves[move].available.totalAttacks;        
+    }
+    function reduceAvailablility4(move) {
+        charMoves[move].available.remainingAttacks -= 1;
+        button4.innerText = charMoves[move].name+" "+charMoves[move].available.remainingAttacks+" "+"/"+charMoves[move].available.totalAttacks;        
+    }
+
+    button1.innerText = charMoves[0].name+" "+charMoves[0].available.remainingAttacks+" "+"/"+charMoves[0].available.totalAttacks;
+    button1.addEventListener('click', function() {
+        reduceAvailablility(0);
+    });
+    button2.innerText = charMoves[1].name+" "+charMoves[1].available.remainingAttacks+" "+"/"+charMoves[1].available.totalAttacks;
+    button2.addEventListener('click', function() {
+        reduceAvailablility2(1);
+    });
+    button3.innerText = charMoves[2].name+" "+charMoves[2].available.remainingAttacks+" "+"/"+charMoves[2].available.totalAttacks;
+    button3.addEventListener('click', function() {
+        reduceAvailablility3(2);
+    });
+    button4.innerText = charMoves[3].name+" "+charMoves[3].available.remainingAttacks+" "+"/"+charMoves[3].available.totalAttacks;
+    button4.addEventListener('click', function() {
+        reduceAvailablility4(3);
+    });
+
 }else if(cookieParse.pokemonName == "Squirtle") {
     // SQUIRTLE MOVESET
     var squirtleMoves = [
         {
         name: "Water Gun",
+        available: {
+            remainingAttacks: 10,
+            totalAttacks: 10
+            }
         },
         {
         name: "Surf",
+        available: {
+            remainingAttacks: 5,
+            totalAttacks: 5
+            }
         },
         {
         name: "Tail Spin",
+        available: {
+            remainingAttacks: 10,
+            totalAttacks: 10
+            }
         },
         {
         name: "Hydro Cannon",
+        available: {
+            remainingAttacks: 5,
+            totalAttacks: 5
+            }
         }
     ]
-    button1.innerText = squirtleMoves[0].name;
-    button2.innerText = squirtleMoves[1].name;
-    button3.innerText = squirtleMoves[2].name;
-    button4.innerText = squirtleMoves[3].name;
+
+    function reduceAvailablility(move) {
+        squirtleMoves[move].available.remainingAttacks -= 1;
+        button1.innerText = squirtleMoves[move].name+" "+squirtleMoves[move].available.remainingAttacks+" "+"/"+squirtleMoves[move].available.totalAttacks;
+    }
+    function reduceAvailablility2(move) {
+        squirtleMoves[move].available.remainingAttacks -= 1;
+        button2.innerText = squirtleMoves[move].name+" "+squirtleMoves[move].available.remainingAttacks+" "+"/"+squirtleMoves[move].available.totalAttacks;
+    }
+    function reduceAvailablility3(move) {
+        squirtleMoves[move].available.remainingAttacks -= 1;
+        button3.innerText = squirtleMoves[move].name+" "+squirtleMoves[move].available.remainingAttacks+" "+"/"+squirtleMoves[move].available.totalAttacks;
+    }
+    function reduceAvailablility4(move) {
+        squirtleMoves[move].available.remainingAttacks -= 1;
+        button4.innerText = squirtleMoves[move].name+" "+squirtleMoves[move].available.remainingAttacks+" "+"/"+squirtleMoves[move].available.totalAttacks;
+    }
+
+    button1.innerText = squirtleMoves[0].name+" "+squirtleMoves[0].available.remainingAttacks+" "+"/"+squirtleMoves[0].available.totalAttacks;
+    button1.addEventListener('click', function() {
+        reduceAvailablility(0);
+    });
+    button2.innerText = squirtleMoves[1].name+" "+squirtleMoves[1].available.remainingAttacks+" "+"/"+squirtleMoves[1].available.totalAttacks;
+    button2.addEventListener('click', function() {
+        reduceAvailablility2(1);
+    });
+    button3.innerText = squirtleMoves[2].name+" "+squirtleMoves[2].available.remainingAttacks+" "+"/"+squirtleMoves[2].available.totalAttacks;
+    button3.addEventListener('click', function() {
+        reduceAvailablility3(2);
+    });
+    button4.innerText = squirtleMoves[3].name+" "+squirtleMoves[3].available.remainingAttacks+" "+"/"+squirtleMoves[3].available.totalAttacks;
+    button4.addEventListener('click', function() {
+        reduceAvailablility4(3);
+    });
 }
